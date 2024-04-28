6f571c31e509ceeba06b5fa2c52fe5cc8783b49d
Fernando Cardoso <fc54413@alunos.fc.ul.pt>
2024-04-28 09:35:06 +0100

    faltar testar no appserver

--- src/app/websites/websites.component.ts
+++ src/app/websites/websites.component.ts
@@ -17,7 +17,9 @@ export class WebsitesComponent {
   websites: Website[] = [];
 
   urls: Url[] = [];
-new: any;
+  new: any;
+  filteredWebsites: Website[] = [];
+  sortingOption: string = 'data_registo';
 
   constructor(private websiteService: WebsiteService){}
 
@@ -35,10 +37,25 @@ new: any;
 
   getWebsites(): void{
     this.websiteService.getWebsites().subscribe(x => this.websites = x);
+    this.applyFilterAndSort();
+  }
+
+  applyFilterAndSort(): void {
+    this.filteredWebsites.sort((a, b) => {
+
+      if (this.sortingOption === 'data_registo') {
+        return new Date(b.data_registo).getTime() - new Date(a.data_registo).getTime();
+      } else if (this.sortingOption === 'ultima_aval') {
+        const aDate = a.url?.ultima_aval ? new Date(a.url.ultima_aval).getTime() : 0;
+        const bDate = b.url?.ultima_aval ? new Date(b.url.ultima_aval).getTime() : 0;
+        return bDate - aDate;
+      } else {
+        return 0;
+      }
+    });
   }
   
   isValidUrl(url: string): boolean {
-    // Expressão regular para validar URLs
     const urlPattern = /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/i;
     return urlPattern.test(url);
   }
