9caac0b727ec93d4b71c1ab1f694f5158239091b
André <fc58211@alunos.fc.ul.pt>
2024-05-24 15:33:12 +0100

    erros de acessibilidade já funcionam mas so aparecem dps de dar refresh

--- src/app/website.service.ts
+++ src/app/website.service.ts
@@ -11,8 +11,8 @@ import { Repo } from './repo';
 })
 export class WebsiteService {
 
-  httpURL = "http://10.101.151.25:3058/catalog"
-  //httpURL = "http://localhost:3000/catalog"
+  //httpURL = "http://10.101.151.25:3058/catalog"
+  httpURL = "http://localhost:3000/catalog"
 
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
